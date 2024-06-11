f9adaee24be249379bc9dff17e399e9116d42ae7
Sungman You <sungman.you@gmail.com>
2024-06-12 00:10:54 +0900

    Create init_.el

---
+++ init_.el
@@ -0,0 +1,19 @@
+;; (setq default-directory "C:/Users/USER/code/")
+(tool-bar-mode -1)
+(toggle-frame-maximized)
+(global-display-line-numbers-mode 1)
+(global-hl-line-mode t)
+(setq inhibit-startup-message t)
+(load-theme 'zenburn t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(chatgpt-shell zenburn-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Fira Code Retina" :foundry "outline" :slant normal :weight regular :height 158 :width normal)))))
